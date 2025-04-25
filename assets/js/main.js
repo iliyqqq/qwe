713bedb934e177544e4257fc69f06e69bcacb811
iliyqqq <zapaska200532@gmail.com>
2025-04-25 20:47:06 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -133,6 +133,20 @@
 				});
 			});
 
+			const scrollToTopButton = document.getElementById('scrollToTopButton');
+
+			window.onscroll = function() {
+    				if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        				scrollToTopButton.style.display = 'block';
+    				} else {
+        				scrollToTopButton.style.display = 'none';
+    				}
+			};
+
+			scrollToTopButton.onclick = function() {
+    				window.scrollTo({ top: 0, behavior: 'smooth' });
+			};
+
 			breakpoints.on('<=small', function() {
 
 				$header.unscrollex();
